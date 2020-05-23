8fb09127a01751030c67fa57dd3883c39b5776a1
Ultrajet <51157704+Ultrajet@users.noreply.github.com>
2020-05-23 13:54:12 +0200

    fix: ajout (à nouveau) d'une migration de created_at pour user

--- src/Migrations/Version20200523115047.php
+++ src/Migrations/Version20200523115047.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20200523105516 extends AbstractMigration
+final class Version20200523115047 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -22,7 +22,7 @@ final class Version20200523105516 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE user ADD pays_naissance VARCHAR(255) DEFAULT NULL, ADD date_deces DATE DEFAULT NULL, ADD ville_deces VARCHAR(255) DEFAULT NULL, ADD pays_deces VARCHAR(255) DEFAULT NULL, CHANGE date date_naissance DATE DEFAULT NULL, CHANGE lieu ville_naissance VARCHAR(255) DEFAULT NULL');
+        $this->addSql('ALTER TABLE user ADD created_at DATETIME NOT NULL');
     }
 
     public function down(Schema $schema) : void
@@ -30,6 +30,6 @@ final class Version20200523105516 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE user ADD date DATE DEFAULT NULL, ADD lieu VARCHAR(255) CHARACTER SET utf8mb4 DEFAULT NULL COLLATE `utf8mb4_unicode_ci`, DROP date_naissance, DROP ville_naissance, DROP pays_naissance, DROP date_deces, DROP ville_deces, DROP pays_deces');
+        $this->addSql('ALTER TABLE user DROP created_at');
     }
 }
