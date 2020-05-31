21c50276bfc9a0f6a3d0968fca7aa3e29c1ea7fc
bart <bldlr170289@gmail.com>
2020-05-31 11:38:18 +0200

    homepage
    page de présentation

--- src/Controller/ListeController.php
+++ src/Controller/ListeController.php
@@ -11,7 +11,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class ListeController extends AbstractController
 {
     /**
-     * @Route("/", name="listeMembres")
+     * @Route("/liste_membres", name="listeMembres")
      */
     public function listeMembres(UserRepository $repoUser)
     {
