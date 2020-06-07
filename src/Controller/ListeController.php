487476a241b4c9d46345444ff215365b0bb9b268
bart <bldlr170289@gmail.com>
2020-06-07 15:33:51 +0200

    perte d'une variable lors du conflit
    recuperation de la variable moisNaissance et moisDeces

--- src/Controller/ListeController.php
+++ src/Controller/ListeController.php
@@ -16,24 +16,26 @@ class ListeController extends AbstractController
     /**
      * @Route("/liste_membres", name="listeMembres")
      */
-    public function listeMembres(UserRepository $repoUser,  Request $request)
+    public function listeMembres(UserRepository $repoUser,  Request $request, DateFr $dateFr)
     {
         $data = new SearchData();
         $form = $this->createForm(SearchForm::class, $data);
         $form->handleRequest($request);
 
         $users = $repoUser->findSearch($data);
-
+        $moisNaissance = $dateFr->moisNaissanceFr($users);
+        $moisDeces = $dateFr->moisDecesFr($users);
         $number = count($repoUser->findSearch($data));
         return $this->render('liste/liste.html.twig',[
             'users' => $users,
             'form' => $form->createView(),
-            'number' => $number
+            'number' => $number,
+            'moisNaissance' => $moisNaissance,
+            'moisDeces' => $moisDeces,
         ]
         );
     }
 
-    
 
 
 }
